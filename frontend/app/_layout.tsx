890b2ab86c2695a79eb57b3c07b93ee8a6ed9de1
Naveen Viswa <naveenviswa1626@gmail.com>
2025-12-16 15:51:47 +0530

    Changes made in layouts

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stack } from "expo-router";
+import Toast from "react-native-toast-message"; // 👈 Import it
 
 export default function RootLayout() {
   return (
+    <>
     <Stack
       screenOptions={{
         headerShown: false, // 🔥 IMPORTANT
       }}
     />
+    <Toast />
+    </>
   );
 }
